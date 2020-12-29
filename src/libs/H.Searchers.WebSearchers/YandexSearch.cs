82f6b4d49f5aecfd8cf6f910104f3bae76f5ad57
Konstantin S <HavenDV@users.noreply.github.com>
2020-12-29 20:29:07 +1000

    Refactored.

--- src/libs/H.Searchers.WebSearchers/YandexSearch.cs
+++ src/libs/H.Searchers.WebSearchers/YandexSearch.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using H.Core;
@@ -13,6 +15,14 @@ namespace H.Searchers
     /// </summary>
     public class YandexSearcher : Module, ISearcher
     {
+        #region Properties
+
+        private HttpClient HttpClient { get; } = new();
+
+        #endregion
+
+        #region Methods
+
         /// <summary>
         /// 
         /// </summary>
@@ -21,9 +31,10 @@ namespace H.Searchers
         /// <returns></returns>
         public async Task<ICollection<SearchResult>> SearchAsync(string query, CancellationToken cancellationToken = default)
         {
-            var url = $"https://www.yandex.ru/search/?text={query}";
-            var web = new HtmlWeb();
-            var document = await web.LoadFromWebAsync(url, cancellationToken);
+            var html = await GetHtmlAsync(query, cancellationToken).ConfigureAwait(false);
+
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
 
             return document.DocumentNode
                 .SelectNodes("//a[@href]")
@@ -32,5 +43,43 @@ namespace H.Searchers
                 .Select(i => new SearchResult(i, string.Empty))
                 .ToList();
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<string> GetHtmlAsync(string query, CancellationToken cancellationToken = default)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, new Uri($"https://www.yandex.ru/search/?text={query}&lr=76"))
+            {
+                Headers =
+                {
+                    { "user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36" },
+                    { "device-memory", "8" },
+                    { "downlink", "3.4" },
+                    { "dpr", "2.25" },
+                    { "ect", "4g" },
+                    { "rtt", "100" },
+                    { "viewport-width", "1138" },
+                }
+            };
+            using var response = await HttpClient.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Dispose()
+        {
+            HttpClient.Dispose();
+        }
+
+        #endregion
     }
 }
